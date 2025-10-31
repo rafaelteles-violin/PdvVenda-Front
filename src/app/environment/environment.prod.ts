606015e02d5117df9ceabf809c9dc88c4bbd578e
Rafael Teles <ralf_teles@hotmail.com>
2025-10-31 15:20:43 -0300

    ajuste

--- src/app/environment/environment.prod.ts
+++ src/app/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-    // urlApi: "https://rafaelteles-002-site1.jtempurl.com/api/"
-     urlApi: "http://localhost:5087/api/"
+     urlApi: "https://rafaelteles-002-site3.jtempurl.com/api/"
+     //urlApi: "http://localhost:5087/api/"
 };
--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  //urlApi: "https://rafaelteles-002-site1.jtempurl.com/api/"
-   urlApi: "http://localhost:5087/api/"
+  urlApi: "https://rafaelteles-002-site3.jtempurl.com/api/"
+   //urlApi: "http://localhost:5087/api/"
 }; 
     
